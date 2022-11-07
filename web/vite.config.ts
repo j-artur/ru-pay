fc3f89b6b9ce3ac9a6e6a128e6bf46633d3edfca
j-artur <jartur.dev@gmail.com>
2022-11-07 19:19:33 -0300

    PWA

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "icon192.png",
+        "icon512.png",
+      ],
+      manifest: {
+        name: "RU Pay - Cliente",
+        short_name: "RU Pay",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
 })

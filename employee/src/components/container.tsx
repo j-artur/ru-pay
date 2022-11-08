7cda1dfafe55fe3d58ddb045f8a374566d9134da
j-artur <jartur.dev@gmail.com>
2022-11-08 03:57:48 -0300

    container padding

--- employee/src/components/container.tsx
+++ employee/src/components/container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="overflow-y-auto">
+    <div className="overflow-y-auto px-4">
       <div className="m-auto max-w-lg min-h-full">{children}</div>
     </div>
   )
